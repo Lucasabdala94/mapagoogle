80c69cf68749512b1e35400eeeaad91b38d814b6
Lucasabdala94 <83674006+Lucasabdala94@users.noreply.github.com>
2022-09-05 09:30:29 -0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './style.css';
+import Mapa from './src/Mapa';
+
+export default function App() {
+  return (
+    <div>
+      <Mapa />
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-jp7amv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-jp7amv)
